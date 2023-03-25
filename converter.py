8385b607a839e88efb3be158e0be13da37b52cb1
jhohannesK <jdomeh77@gmail.com>
2023-03-25 09:21:07 +0000

    update convertor

--- converter.py
+++ converter.py
@@ -2,7 +2,10 @@ import PySimpleGUI as sg
 
 layout = (
     [
-        [sg.Text("Enter minute:", enable_events=True, key="-TEXT-")],
+        [
+            sg.Text("Choose coversion to perform:", enable_events=True, key="-TEXT-"),
+            sg.Combo(["Seconds to Minutes", "Minutes to Seconds"], key="-COMBO-"),
+        ],
         [sg.InputText(key="-INPUT-")],
         [sg.Button("Convert", key="-CONVERT-")],
         [sg.Text(key="-HELLO-")],
@@ -13,12 +16,27 @@ window = sg.Window("Converter", layout)
 
 while True:
     event, values = window.read()
-    convertedValue = int(values["-INPUT-"]) * 60
     if event == sg.WIN_CLOSED:
         break
 
     if event == "-CONVERT-":
-        window["-HELLO-"].update("The value in seconds is " + str(convertedValue))
+        inputValue = values["-INPUT-"]
+        if values["-COMBO-"] == "":
+            window["-HELLO-"].update("Click the dropdown to select a conversion")
+        elif inputValue.isnumeric():
+            convertedValuetoSeconds = int(values["-INPUT-"]) * 60
+            convertedValuetoMinutes = int(values["-INPUT-"]) / 60
+            match values["-COMBO-"]:
+                case "Seconds to Minutes":
+                    window["-HELLO-"].update(
+                        "The value in minutes is " + str(convertedValuetoMinutes)
+                    )
+                case "Minutes to Seconds":
+                    window["-HELLO-"].update(
+                        "The value in seconds is " + str(convertedValuetoSeconds)
+                    )
+        else:
+            window["-HELLO-"].update("Please enter a valid number")
 
     if event == "-TEXT-":
         print("text has been pressed")
